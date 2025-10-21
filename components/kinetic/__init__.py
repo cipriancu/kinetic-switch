91229c25de83ccacade480f540d28f6db85d9cee
cipriancu <cipriancu@yahoo.com>
2025-10-21 15:52:51 +0100

    Refactor Kinetic component to use new class structure

--- components/kinetic/__init__.py
+++ components/kinetic/__init__.py
@@ -1,21 +1,27 @@
 import esphome.codegen as cg
+from esphome.components import remote_base, remote_receiver, sensor
 import esphome.config_validation as cv
-from esphome.components import sensor, remote_receiver
 from esphome.const import CONF_ID
 
-kinetic_ns = cg.esphome_ns.namespace("kinetic")
-KineticSwitchSensor = kinetic_ns.class_("KineticSwitchSensor", sensor.Sensor, cg.Component)
+CODEOWNERS = ["@cipriancu"]
+DEPENDENCIES = ["remote_receiver"]
+AUTO_LOAD = ["sensor"]
 
-CONF_RECEIVER_ID = "receiver_id"
+kinetic_ns = cg.esphome_ns.namespace("kinetic")
+KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component, remote_base.RemoteReceiverListener)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(KineticSwitchSensor),
-    cv.Required(CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
-})
+    cv.GenerateID(): cv.declare_id(KineticComponent),
+    cv.Required(remote_base.CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
+    cv.Optional("id_sensor"): sensor.sensor_schema(),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    rcvr = await cg.get_variable(config[CONF_RECEIVER_ID])
-    cg.add(var.set_remote_receiver(rcvr))
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
+    await remote_base.register_listener(var, config)
+
+    if "id_sensor" in config:
+        sens = await sensor.new_sensor(config["id_sensor"])
+        cg.add(var.set_id_sensor(sens))
+
