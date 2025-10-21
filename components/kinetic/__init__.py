4708a2ada9a7093c3f65d9cad4b268d82a65851c
cipriancu <cipriancu@yahoo.com>
2025-10-21 15:42:52 +0100

    Refactor kinetic component class and config schema

--- components/kinetic/__init__.py
+++ components/kinetic/__init__.py
@@ -1,3 +1,4 @@
+# __init__.py
 import esphome.codegen as cg
 from esphome.components import remote_base, remote_receiver
 import esphome.config_validation as cv
@@ -7,17 +8,12 @@ CODEOWNERS = ["@cipriancu"]
 DEPENDENCIES = ["remote_receiver"]
 
 kinetic_ns = cg.esphome_ns.namespace("kinetic")
-kineticComponent = kinetic_ns.class_("kineticComponent", cg.Component)
+KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component, remote_base.RemoteReceiverListener)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(kineticComponent),
-        cv.GenerateID(remote_base.CONF_RECEIVER_ID): cv.use_id(
-            remote_receiver.RemoteReceiverComponent
-        ),
-    }
-)
-
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(KineticComponent),
+    cv.Required(remote_base.CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
