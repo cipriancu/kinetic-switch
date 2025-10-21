75b54acc92c3a47a01ca861d5863e7e185c6fa4a
cipriancu <cipriancu@yahoo.com>
2025-10-21 15:44:18 +0100

    Refactor Kinetic component and remove unused classes
    Refactor Kinetic component to improve structure and clarity.

--- components/kinetic/kinetic.h
+++ components/kinetic/kinetic.h
@@ -1,26 +1,23 @@
+#pragma once
 #include "esphome/core/component.h"
+#include "esphome/components/remote_base/remote_base.h"
 #include "esphome/components/remote_receiver/remote_receiver.h"
 #include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace kinetic {
 
-// Definim clasa decodorului care mosteneste RemoteReceiveData
-class MyKineticSwitchDecoder : public remote_receiver::RemoteReceiveData {
+// Component care ascultă semnale radio și raportează ID-ul unui switch
+class KineticComponent : public Component, public remote_base::RemoteReceiverListener {
  public:
-  bool decode(remote_receiver::RemoteReceiveData *data) override;
+  sensor::Sensor *id_sensor_{nullptr};
+
+  void setup() override;
+  void dump_config() override;
+  void on_receive(remote_base::RemoteReceiveData data) override;
+  void set_id_sensor(sensor::Sensor *sensor) { id_sensor_ = sensor; }
 };
 
-// Definim un senzor care va raporta ID-ul (il vom trata ca un senzor text)
-class KineticSwitchSensor : public sensor::Sensor, public Component {
- public:
-  void set_remote_receiver(remote_receiver::RemoteReceiverComponent *rcvr) { 
-    rcvr->add_decoder(new MyKineticSwitchDecoder(this)); 
-  }
-  
-  // Functie pentru a actualiza valoarea senzorului dupa o decodare reusita
-  void publish_id(uint32_t id_value); 
-};
+}  // namespace kinetic
+}  // namespace esphome
 
-} // namespace my_rf_decoder
-} // namespace esphome
