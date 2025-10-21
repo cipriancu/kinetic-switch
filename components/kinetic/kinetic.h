d825b1ca661119a7610511d26bddf01b321ede4e
cipriancu <cipriancu@yahoo.com>
2025-10-21 17:10:49 +0100

    Refactor KineticComponent to use RemoteBase
    Updated KineticComponent to implement RemoteReceiverListener and changed remote receiver handling.

--- components/kinetic/kinetic.h
+++ components/kinetic/kinetic.h
@@ -1,20 +1,24 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/remote_receiver/remote_receiver.h"
+#include "esphome/components/remote_base/remote_base.h" // NOU!
 #include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace kinetic {
 
-class KineticComponent : public Component {
+class KineticComponent : public Component, public remote_base::RemoteReceiverListener { // Implementam interfața
  public:
-  void set_remote_receiver(remote_receiver::RemoteReceiverComponent *rcvr) { rcvr_ = rcvr; }
-  void loop() override;
-  void publish_id(uint32_t id_value);
+  // Functia necesara din RemoteReceiverListener
+  void on_receive(const remote_base::RemoteReceiveData &data) override; 
+  
+  void set_id_sensor(sensor::Sensor *sens) { this->id_sensor_ = sens; }
+  void loop() override {} // Nu mai avem nevoie de logica in loop
 
  protected:
-  remote_receiver::RemoteReceiverComponent *rcvr_{nullptr};
+  void publish_id(uint32_t id_value);
+
+  sensor::Sensor *id_sensor_{nullptr};
   uint32_t last_id_{0};
 };
 
