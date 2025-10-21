f71137d9d8a990143935b53b3b6d672a7045f1c2
cipriancu <cipriancu@yahoo.com>
2025-10-21 15:44:43 +0100

    Add sensor support to kinetic component

--- components/kinetic/__init__.py
+++ components/kinetic/__init__.py
@@ -1,11 +1,11 @@
-# __init__.py
 import esphome.codegen as cg
-from esphome.components import remote_base, remote_receiver
+from esphome.components import remote_base, remote_receiver, sensor
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@cipriancu"]
 DEPENDENCIES = ["remote_receiver"]
+AUTO_LOAD = ["sensor"]
 
 kinetic_ns = cg.esphome_ns.namespace("kinetic")
 KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component, remote_base.RemoteReceiverListener)
@@ -13,9 +13,15 @@ KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component, remote_ba
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(KineticComponent),
     cv.Required(remote_base.CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
+    cv.Optional("id_sensor"): sensor.sensor_schema(),
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await remote_base.register_listener(var, config)
+
+    if "id_sensor" in config:
+        sens = await sensor.new_sensor(config["id_sensor"])
+        cg.add(var.set_id_sensor(sens))
+
