eaacd8b884016723693f2f95a3f275357b363adb
cipriancu <cipriancu@yahoo.com>
2025-10-21 14:57:04 +0100

    Update __init__.py

--- components/my_rf_decoder/__init__.py
+++ components/my_rf_decoder/__init__.py
@@ -1,21 +1,26 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import remote_receiver, sensor
+from esphome.components import remote_receiver, remote_base
 from esphome.const import CONF_ID
 from esphome.components import remote_receiver
 from esphome.components.remote_receiver import CONF_REMOTE_RECEIVER # Importul corect
 
-CODEOWNERS = ["cipriancu"]
+CODEOWNERS = ["@cipriancu"]
+DEPENDENCIES = ["remote_receiver"]
 
 # Definim namespace-ul C++ si clasa Sensorului
 my_rf_decoder_ns = cg.esphome_ns.namespace('my_rf_decoder')
-KineticSwitchSensor = my_rf_decoder_ns.class_('KineticSwitchSensor', sensor.Sensor, cg.Component)
+KineticSwitchSensor = my_rf_decoder_ns.class_('KineticSwitchSensor', cg.Component)
 
 # Schema de configurare YAML
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(KineticSwitchSensor),
-    cv.Required(CONF_REMOTE_RECEIVER): cv.use_id(remote_receiver.RemoteReceiverComponent),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(KineticSwitchSensor),
+        cv.GenerateID(remote_base.CONF_RECEIVER_ID): cv.use_id(
+            remote_receiver.RemoteReceiverComponent
+        ),
+    }
+)
 
 def to_code(config):
     # Generam codul pentru componenta noastra (senzorul)
