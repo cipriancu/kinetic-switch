9d8abaf82bc2f5eb380fc02890c700d3f7e54069
cipriancu <cipriancu@yahoo.com>
2025-10-21 16:01:14 +0100

    Refactor KineticComponent to remove remote_base dependency

--- components/kinetic/kinetic.h
+++ components/kinetic/kinetic.h
@@ -1,29 +1,21 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/remote_base/remote_base.h"
 #include "esphome/components/remote_receiver/remote_receiver.h"
 #include "esphome/components/sensor/sensor.h"
-#include <vector>
-#include <cstdint>
 
 namespace esphome {
 namespace kinetic {
 
-class KineticComponent : public Component, public remote_base::RemoteReceiverListener {
+class KineticComponent : public Component {
  public:
-  // Pointer către senzorul opțional (poate fi nullptr)
-  sensor::Sensor *id_sensor_{nullptr};
+  void set_remote_receiver(remote_receiver::RemoteReceiverComponent *rcvr) { rcvr_ = rcvr; }
+  void loop() override;
+  void publish_id(uint32_t id_value);
 
-  void setup() override;
-  void dump_config() override;
-  void on_receive(const remote_base::RemoteReceiveData &data) override;
-
-  void set_id_sensor(sensor::Sensor *sensor) { id_sensor_ = sensor; }
-
- private:
-  // Returnează true dacă decodează un ID valid (25 biți)
-  bool decode_kinetic(const std::vector<uint32_t> &timings, uint32_t &id_value);
+ protected:
+  remote_receiver::RemoteReceiverComponent *rcvr_{nullptr};
+  uint32_t last_id_{0};
 };
 
 }  // namespace kinetic
