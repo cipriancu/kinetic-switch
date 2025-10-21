b682291706609f7a314b784a4a9751b46802dfc0
cipriancu <cipriancu@yahoo.com>
2025-10-21 15:53:17 +0100

    Refactor on_receive method signature
    Updated on_receive method to take a const reference.

--- components/kinetic/kinetic.h
+++ components/kinetic/kinetic.h
@@ -1,23 +1,28 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/remote_base/remote_base.h"
 #include "esphome/components/remote_receiver/remote_receiver.h"
 #include "esphome/components/sensor/sensor.h"
+#include <vector>
+#include <cstdint>
 
 namespace esphome {
 namespace kinetic {
 
 class KineticComponent : public Component, public remote_base::RemoteReceiverListener {
  public:
+  // Pointer către senzorul opțional (poate fi nullptr)
   sensor::Sensor *id_sensor_{nullptr};
 
   void setup() override;
   void dump_config() override;
-  void on_receive(remote_base::RemoteReceiveData data) override;
+  void on_receive(const remote_base::RemoteReceiveData &data) override;
 
   void set_id_sensor(sensor::Sensor *sensor) { id_sensor_ = sensor; }
 
  private:
+  // Returnează true dacă decodează un ID valid (25 biți)
   bool decode_kinetic(const std::vector<uint32_t> &timings, uint32_t &id_value);
 };
 
