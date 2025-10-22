3ce30c64c109d3a684b395572b434cc8a66306e8
cipriancu <cipriancu@yahoo.com>
2025-10-22 15:22:41 +0100

    Update __init__.py

--- components/kinetic/__init__.py
+++ components/kinetic/__init__.py
@@ -7,7 +7,7 @@ CODEOWNERS = ["@cipriancu"]
 DEPENDENCIES = ["remote_receiver"]
 
 kinetic_ns = cg.esphome_ns.namespace("kinetic")
-KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component)
+KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component, remote_base.RemoteReceiverListener)
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -22,4 +22,4 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await remote_base.register_listener(var, config)
+    await remote_base.register_listener(var)
