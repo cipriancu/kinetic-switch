8e57374692b16c95efe2827aa1c6d8cca9f5cfba
cipriancu <cipriancu@yahoo.com>
2025-10-22 14:45:08 +0100

    Refactor KineticComponent to remove sensor support
    Removed sensor dependency and related configuration.

--- components/kinetic/__init__.py
+++ components/kinetic/__init__.py
@@ -1,27 +1,25 @@
 import esphome.codegen as cg
-from esphome.components import remote_base, remote_receiver, sensor
+from esphome.components import remote_base, remote_receiver
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@cipriancu"]
 DEPENDENCIES = ["remote_receiver"]
-AUTO_LOAD = ["sensor"]
 
 kinetic_ns = cg.esphome_ns.namespace("kinetic")
-KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component, remote_base.RemoteReceiverListener)
+KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(KineticComponent),
-    cv.Required(remote_base.CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
-    cv.Optional("id_sensor"): sensor.sensor_schema(),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(KineticComponent),
+        cv.GenerateID(remote_base.CONF_RECEIVER_ID): cv.use_id(
+            remote_receiver.RemoteReceiverComponent
+        ),
+    }
+)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.add_library("Sensor", None)
+    await cg.register_component(var, config)
     await remote_base.register_listener(var, config)
-
-    if "id_sensor" in config:
-        sens = await sensor.new_sensor(config["id_sensor"])
-        cg.add(var.set_id_sensor(sens))
-
