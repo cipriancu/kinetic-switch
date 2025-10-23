2b87c1e19158fce81e27d83ed0b805e2c3e023f6
cipriancu <cipriancu@yahoo.com>
2025-10-23 08:25:50 +0100

    Refactor KineticComponent methods and return type
    Updated KineticComponent methods to provide default implementations and changed on_receive to return a boolean.

--- components/kinetic/kinetic.h
+++ components/kinetic/kinetic.h
@@ -8,14 +8,10 @@ namespace kinetic {
 
 class KineticComponent : public Component, public remote_base::RemoteReceiverListener {
  public:
-  void setup() override;
-  void dump_config() override;
-  void on_receive(remote_base::RemoteReceiveData data) override;
-
- protected:
-  void handle_kinetic_code(uint32_t code);
+  void setup() override {}
+  void loop() override {}
+  bool on_receive(remote_base::RemoteReceiveData data) override;  // <== bool, nu void!
 };
 
 }  // namespace kinetic
 }  // namespace esphome
-
