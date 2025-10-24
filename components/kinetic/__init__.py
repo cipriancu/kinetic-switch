2509913e24d595b2c0f3124769c1c2bb46e39256
cipriancu <cipriancu@yahoo.com>
2025-10-24 08:40:27 +0100

    Update __init__.py

--- components/kinetic/__init__.py
+++ components/kinetic/__init__.py
@@ -1,20 +1,24 @@
 import esphome.codegen as cg
+from esphome.components import remote_base, remote_receiver
 import esphome.config_validation as cv
-from esphome.components import remote_receiver
 from esphome.const import CONF_ID
 
-AUTO_LOAD = ["remote_base"]
+CODEOWNERS = ["@cipriancu"]
+DEPENDENCIES = ["remote_receiver"]
 
 kinetic_ns = cg.esphome_ns.namespace("kinetic")
-RemoteReceiverListener = remote_receiver.remote_base_ns.class_("RemoteReceiverListener")
+KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component)
 
-KineticComponent = kinetic_ns.class_("KineticComponent", cg.Component, RemoteReceiverListener)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(KineticComponent),
-}).extend(remote_receiver.REMOTE_RECEIVER_SCHEMA)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(KineticComponent),
+        cv.GenerateID(remote_base.CONF_RECEIVER_ID): cv.use_id(
+            remote_receiver.RemoteReceiverComponent
+        ),
+    }
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await remote_receiver.register_remote_receiver_listener(var, config)
+    await remote_base.register_listener(var, config)
